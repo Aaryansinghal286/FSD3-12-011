82a8505ba3fd8b8f1875f439799953ffa48a2254
Aaryansinghal286 <aaryansinghal28@gmail.com>
2026-07-31 14:36:27 +0530

    changes in fun2

--- lab-1/eventEmitter.js
+++ lab-1/eventEmitter.js
@@ -1,1 +1,16 @@
- 
+const login = (name) => {
+    console.log(`${name} is logged in`);
+};
+
+const start=() => {
+    console.log("System starts");
+};
+const working = (name) => {
+    console.log(`${name} add items to cart`);
+};
+const checkout = (name) => {
+    console.log(`${name} logged out`);
+};
+
+login('Aaryan Singhal');
+start();

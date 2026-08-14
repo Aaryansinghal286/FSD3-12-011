78ac14a7929ce38f283a52125c6d634b1d862b7a
Aaryansinghal286 <aaryansinghal28@gmail.com>
2026-08-14 15:42:04 +0530

    crud project update

--- lab-2/crud.js
+++ lab-2/crud.js
@@ -2,6 +2,34 @@ import readline from "readline/promises";
 import {stdin, stdout} from "process";
 import {readFile, writeFile} from "fs/promises";
 
+const FILE = "product.json";
+
+const getCart = async () => {
+    const data = await readFile(FILE, "utf-8");
+    return JSON.parse(data);
+};
+
+const saveCart = async (myCart) => {
+    await writeFile(FILE, JSON.stringify(myCart, null, 2));
+};
+
+const addToCart = async (product) => {
+    const myCart = await getCart();
+    const isFound = myCart.find((item) => item.id === product.id);
+    if (isFound) {
+        isFound.qty += product.qty;
+    } else {
+        myCart.push(product);
+    }
+    await saveCart(myCart);
+    console.log(`Product added/updated with id ${product.id} into cart`);
+};
+
+const showCart = async () => {
+    const data = await getCart();
+    console.table(data);
+}
+
 const main= async () => {
     let choice;
     const cin = readline.createInterface({input:stdin, output:stdout});
@@ -11,22 +39,25 @@ const main= async () => {
         console.log("2.............Add Product");
         console.log("3.............Remove Product");
         console.log("4.............Update Product");
-        console.log("5.............Exit🛩️");
+        console.log("5.............checkOut🛩️");
         choice = await cin.question("Enter your choice: ");
-        switch(choice){
-            case "1":
+        switch(Number(choice)){
+            case 1:
                 console.log("Show Products");
                 break;
-            case "2":
-                console.log("Product added");
+            case 2:
+                let data = await cin.question("Enter ID, name, price, qty: ");
+                const [id, name, price, qty] = data.split(',').map((item) => item.trim());
+                const product = {id: Number(id), name, price: Number(price), qty: Number(qty)};
+                await addToCart(product);
                 break;
-            case "3":
+            case 3:
                 console.log("Remove Product");
                 break;
-            case "4":
+            case 4:
                 console.log("Update Product Quantity");
                 break;
-            case "5":
+            case 5:
                 console.log("Exit, See You Later🛩️");
                 break;
             default:

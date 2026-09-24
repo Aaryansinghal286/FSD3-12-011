61245ab5dc71456abcf56f57b5d16e5fb6e7b4b4
Aaryansinghal286 <aaryansinghal28@gmail.com>
2026-09-24 10:57:21 +0530

    lab2 update

--- lab2/file1.js
+++ lab2/file1.js
@@ -2,7 +2,7 @@ import { writeFile, appendFile, readFile } from "fs/promises";
 
 // await writeFile("hello.txt", "JS is Easy");
 
-// await appendFile("hello.txt", "\nFS is much Easy than others");
+await appendFile("hello.txt", "\nFS is much Easy than others");
 
 // await appendFile("hello.txt", "\nAaryan Singhal 🫩");
 
